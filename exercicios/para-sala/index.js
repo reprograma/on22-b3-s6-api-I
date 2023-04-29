3b020ad57b74c2d47fe2adde2d2582b30b33be13
Gabriela <pgabrielapatricia@gmail.com>
2023-04-29 19:10:58 -0300

    Trabalho de casa concluído em nome de santo expedito!

---
+++ exercicios/para-sala/index.js
@@ -0,0 +1,14 @@
+console.log("A vida é um murangu")
+const livros = require ("./biblioteca.json")
+/*console.log (livros)
+console.log(livros[0])
+const livros1 = livros[0]
+console.log(livros1.nome)
+console.log(livros1.jujuba)*/
+
+livros.forEach((livros)=>{
+    console.log(livros)
+}
+
+)
+
