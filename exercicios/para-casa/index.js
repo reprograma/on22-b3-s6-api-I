b21e51177ed52d4ef774eb415d08a93a4aef255b
LaísMSFerreira <laisferreira-@homail.com>
2023-05-06 01:21:57 -0300

    Exercicio para casa API-s6

--- exercicios/para-casa/index.js
+++ exercicios/para-casa/index.js
@@ -1,10 +1,18 @@
-const itens = require("./cafeteria.json")
+const itensDoCardapio = require("./cafeteria.json")
 
-console.log(itens)
 
-itens.forEach((item) => {
+console.log("💜 Cafeteria da Reprograma 💜")
+console.log("           ----           ")
+console.log("Vai um cafezinho pra não surtar?")
+console.log("           ----           ")
+console.log("Se delicie com o nosso cardápio:")
+
+itensDoCardapio.forEach((item) => {
     console.log(item)
-}
-)
+})
 
-
+itensDoCardapio.forEach(item => {
+        console.log(`${item.vegetariano == true ? "É vegetariano." : "Não é vegetariano."}`)
+        console.log(`${item.vegano == true ? "Bota pra dentro, é vegano." : "Não é vegano."}`)
+        console.log("><><")
+})
