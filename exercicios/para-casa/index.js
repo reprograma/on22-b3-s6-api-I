ea3e72b4807a3ad4311c49f11501b658509a8ff2
Tissianass <tissianass@gmail.com>
2023-05-28 11:57:53 -0300

    add exercicio casa

--- exercicios/para-casa/index.js
+++ exercicios/para-casa/index.js
@@ -1,31 +1,14 @@
-const itens = require("./cafeteria-itens.json")
-const indicacoesEspeciais = require("./indicacoes-especiais.json")
+const itens = require("./espressocode-itens.json")
 
-
-function exibeItem(item) {
+function cardápio(item) {
     console.log(`<${item.nome}>`)
     console.log(`Descrição: ${item.descricao}`)
-    exibeIndicacoesEspeciais(item.indicacoesEspeciais)
-    console.log(`Preço: R$${item["preco total"]}`)
+    console.log(`Indicação: ${item.indicacoesEspeciais}`)
+    console.log (`"R$": ${item.precototal}`)
     console.log(`</>\n`)
-
 }
 
-function exibeIndicacoesEspeciais(indicacoesDoProduto) {
-    let descricaoIndicacoesEspeciais = indicacoesDoProduto.map((indicacaoDoProduto) => {
-        return indicacoesEspeciais
-            .find(indicacaoEspecial => indicacaoEspecial.nome === indicacao)
-            .descricao
-    })
-    
-    console.log(descricaoIndicacoesEspeciais.join(','))
-}
-
-console.log("</Coffee and Code>")
+console.log("</Espresso Code>")
 console.log("\n - Cardápio -\n")
 
-itens.forEach(item => exibeItem(item))
-
-// itens.forEach((item) => {
-//     exibeItem(item)
-// })
+itens.forEach(item => cardápio(item))
